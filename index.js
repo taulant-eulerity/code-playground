fb810118bb7ee4338ab08d95903de00a37bf08fc
taulant-eulerity <taulant@eulerity.com>
2023-03-21 10:15:00 -0400

    app

--- index.js
+++ index.js
@@ -1,73 +1,85 @@
 /* eslint-disable no-lonely-if */
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 const app = express();
-const { Readable } = require('stream');
-const { execFile } = require('child_process');
-//const AWS = require('aws-sdk');
-const { createWriteStream } = require('fs');
-
+const { Readable } = require("stream");
+const { execFile } = require("child_process");
+const AWS = require("aws-sdk");
+const { createWriteStream } = require("fs");
+const axios = require("axios");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+function getIP(req) {
+  const clientIpAddress =
+    (req.headers["x-forwarded-for"] || "").split(",").pop().trim() ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress;
+  console.log(clientIpAddress, "Hey");
+  return clientIpAddress;
+}
+async function getGeolocation(ipAddress) {
+  try {
+    const response = await axios.get(`http://ip-api.com/json/${ipAddress}`);
+    const data = response.data;
 
-// AWS.config.update({
-//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-//   region: 'us-east-1'
-// });
+    if (data.status === "fail") {
+      console.error("Failed to get geolocation data:", data.message);
+      return null;
+    }
 
+    return {
+      lat: data.lat,
+      lon: data.lon,
+      city: data.city,
+      region: data.regionName,
+      country: data.country,
+    };
+  } catch (error) {
+    console.error("Error getting geolocation data:", error.message);
+    return null;
+  }
+}
 
-// function sendEmail() {
-//   // Create an SES instance
-//   const ses = new AWS.SES({ apiVersion: '2010-12-01' });
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: "us-east-1",
+});
 
-//   // Define the email parameters
-//   const emailParams = {
-//     Source: 'taulantus@gmail.com',
-//     Destination: {
-//       ToAddresses: ['taulantus@gmail.com']
-//     },
-//     Message: {
-//       Subject: {
-//         Data: 'New Visitor'
-//       },
-//       Body: {
-//         Text: {
-//           Data: 'Email body text'
-//         },
-//         Html: {
-//           Data: '<p>Someone visited your app</p>'
-//         }
-//       }
-//     }
-//   };
+async function sendSMS(message) {
+  const sns = new AWS.SNS();
 
-//   // Send the email
-//   return ses.sendEmail(emailParams).promise();
-// }
+  const smsParams = {
+    Message: `New Visit: ${message}`,
+    PhoneNumber: "+16463316367",
+  };
 
-
+  return sns.publish(smsParams).promise();
+}
 
 app.get("/", (req, res) => {
-   res.status(200).json("<h1>Success</h1>")
-})
 
-app.post('/testCode', async (req, res) => {
-    // await sendEmail()
+  res.status(200).json("<h1>Success</h1>");
+});
+
+app.post("/testCode", async (req, res) => {
+  const results = await getGeolocation(getIP(req));
+  await sendSMS(JSON.stringify(results));
   try {
     const { script, functionName } = req.body.data;
 
-    const file = createWriteStream('results.js');
+    const file = createWriteStream("results.js");
     const buffer = Buffer.from(script);
     const readable = new Readable();
     readable._read = () => {};
     readable.push(buffer);
     readable.push(null);
-    readable.pipe(file).on('finish', () => {
+    readable.pipe(file).on("finish", () => {
       const findMachJson = (string, { first, second }) => {
         for (let i = 0; i < string.length; i++) {
           let firstChar = string[i];
@@ -78,28 +90,23 @@ app.post('/testCode', async (req, res) => {
         }
       };
 
-      execFile(
-        'npm',
-        ['test', `./tests/${functionName}.test.js`],
-        (_err, bufOut) => {
-          const index = findMachJson(bufOut, { first: '{', second: '"' });
-          if (index) {
-            const JsonData = JSON.parse(bufOut.slice(index));
-            res.json(JsonData);
-            return;
-          }
-          res.status(404).json({ message: 'Not Found' });
+      execFile("npm", ["test", `./tests/${functionName}.test.js`], (_err, bufOut) => {
+        const index = findMachJson(bufOut, { first: "{", second: '"' });
+        if (index) {
+          const JsonData = JSON.parse(bufOut.slice(index));
+          res.json(JsonData);
+          return;
         }
-      );
+        res.status(404).json({ message: "Not Found" });
+      });
     });
   } catch (error) {
     res.status(400).json(error);
   }
 });
 
-app.use(express.static(path.join(__dirname, '/public')));
-
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.listen(8080, (_) => {
-  console.log('Listening');
+  console.log("Listening");
 });
